c052f95f4029483c855e5500db80857b2e25a4d4
onanuviie <nuviesilvet@gmail.com>
2023-03-10 11:32:11 +0100

    made nav bar functional

---
+++ script.js
@@ -0,0 +1,15 @@
+const navMenu = document.querySelector('.nav-menu')
+const hamburger = document.querySelector('.hamburger')
+const closeButton = document.querySelector('.close')
+
+hamburger.addEventListener('click', () => {
+    hamburger.classList.add('active')
+    closeButton.classList.add('active')
+    navMenu.classList.add('active')
+})
+
+closeButton.addEventListener('click', () => {
+    closeButton.classList.remove('active')
+    hamburger.classList.remove('active')
+    navMenu.classList.remove('active')
+})
